91ffe03ef5389a4b448379cd0176a21d6e8e58ab
moyse <moysemei@hotmail.com>
2024-03-07 21:48:56 -0300

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,40 @@
+import { useNavigate } from 'react-router-dom';
+import { RiShutDownLine } from 'react-icons/ri';
+import { useAuth } from '../../hooks/auth';
+import { api } from '../../services/api';
+import { Container, Profile, Logout } from './styles';
+
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg';
+
+export function Header() {
+  const { signOut, user } = useAuth();
+  const navigate = useNavigate();
+
+  function handleSignOut() {
+    navigate("/");
+    signOut();
+  }
+
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+
+
+  return (
+    <Container>
+      <Profile to="/profile">
+        <img 
+        src={avatarUrl}
+        alt={user.name}
+        />
+
+        <div>
+          <span>Bem-vindo</span>
+          <strong>{user.name}</strong>
+        </div>
+      </Profile>
+
+      <Logout onClick={handleSignOut}>
+        <RiShutDownLine />
+      </Logout>
+    </Container>
+  );
+}
